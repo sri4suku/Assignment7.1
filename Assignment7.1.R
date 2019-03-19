daf30b22000d8057a58b57041af37fa93a8704a1
Srikanth K <sri.suku07@gmail.com>
2019-03-20 00:05:38 +0530

    Add files via upload

--- Assignment7.1.R
+++ Assignment7.1.R
@@ -1,21 +1,32 @@
 #1. Write a program to create histograms for all columns
 windows()
-par(mfrow=c(5,4))
-for ( i in 1:length(mtcars)){
-  barplot (height=50,mtcars,main=paste("Histogram of -",colnames(mtcars)),narm=T)
-
-}
-
-dev.off()
-plot(mtcars$gear,type="S")
-
-#2. 
-windows()
-par(mfrow=c(5,4))
+par(mfrow=c(3,4))
 for ( i in 1:length(mtcars)){
   hist(mtcars[,i],main=paste("Histogram of -",colnames(mtcars)[i]),xlab=colnames(mtcars)[i])
-  abline(v = mean(mtcars[,i]), col = "red")
 }
+
+
+#2. Check the probability distribution of all variables in mtcars.
+windows()
+par(mfrow=c(3,4))
+for ( i in 1:length(mtcars)){
+  hist(mtcars[,i],prob=TRUE,main=paste("Prod of -",colnames(mtcars)[i]),xlab=colnames(mtcars)[i]) 
+  lines(density(mtcars[,i],na.rm=T),col="red",lwd=1)  
+}
+
+#other way
+library(tidyr)
+library(purrr)
+library(ggplot2)
+library(magrittr)
+
+mtcars %>%
+  keep(is.numeric) %>%
+  gather() %>%
+  ggplot(aes(value)) +
+  facet_wrap(~key,scales = "free") +
+  stat_density()
+
 
 
 #3.	Write a program to create boxplot for all variables.
